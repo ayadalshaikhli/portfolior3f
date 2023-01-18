2b1c3756b6cce772f3d78a0a8ac1becdeb8fbf58
ayadalshaikhli <79942630+ayadalshaikhli@users.noreply.github.com>
2023-01-17 22:19:50 -0800

    ayad

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,7 @@ export default function IndexPage() {
             </Timeline>
           </Scene>
         </div>
-        <div className="md:hidden">
+        <div className="lg:hidden">
           <Scene triggerHook="onLeave" duration="1200%" pin={true}>
             <Timeline wrapper={<div id="pinContainer" />}>
               <Tween from={{ x: "100%" }} to={{ x: "0%" }}>
